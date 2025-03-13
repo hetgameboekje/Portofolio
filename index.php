fd24440f38541914ef8b3667ffa64ac66e82f859
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2025-03-13 11:08:26 +0100

    update

--- index.php
+++ index.php
@@ -5,35 +5,4 @@ if(isset($_GET['m'])){
     $path = './extention/'.$extention.'/controller.php';
     include $path;
 }
-?><?php
-// index.php
-
-// Enable autoloading
-spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/extentions/' . strtolower($class) . '/controller.php';
-    if (file_exists($path)) {
-        require_once $path;
-    }
-});
-
-// Define routes
-$routes = [
-    'dashboard' => 'DashboardController',
-];
-
-// Get the requested extension from URL
-if (isset($_GET['m'])) {
-    $extension = $_GET['m'];
-    // Validate the extension to prevent directory traversal
-    if (array_key_exists($extension, $routes)) {
-        $controllerClass = $routes[$extension];
-        $controller = new $controllerClass();
-        $controller->index(); // Call the index action by default
-    } else {
-        echo "Invalid extension";
-    }
-} else {
-    // Default route
-    $controller = new DashboardController();
-    $controller->index();
-}
+?>
