e39e01329cb965edbf7d766c67c8a26b20a08b9a
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2025-03-20 21:24:21 +0100

    extensions

--- extension/dashboard/controller.php
+++ extension/dashboard/controller.php
@@ -2,11 +2,18 @@
 
 namespace extension\dashboard;
 
+use extension\dashboardmodel;
+
 class Controller {
     public static function init()
     {
+       
         $url = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
         print_r($url); // Debugging: check URL structure
+        
+    }
+    public function details(){
+        print_r(details_dashboard());
     }
 }
 
--- extension/dashboard/model/dashboardmodel.php
+++ extension/dashboard/model/dashboardmodel.php
@@ -1,9 +1,15 @@
 <?php
 
+namespace extension\dashboardmodel;
+
 class DashboardModel {
     public function getData() {
         // Fetch data from database or other sources
+        $data = 'fun';
         return $data;
+    }
+    public function details_dashboard(){
+        return 'hello world, details';
     }
 }
 
